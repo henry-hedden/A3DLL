fe7ce0b9afeb2a0de621575a9deea52b5aa2ec03
Henry Hedden <hhedden1@mail.middlesex.edu>
2018-03-03 11:12:41 -0500

    Completed code header for `DLL.java`.

--- src/A3DLL/DLL.java
+++ src/A3DLL/DLL.java
@@ -1,28 +1,57 @@
-// Program: 
+// Program: A3DLL
 // Written by: Henry Hedden
-// Program Description: 
-// File name: 
-// File description: 
+// Program Description: The program will implement a Node
+// based Doubly Linked List using Generics.
+// File name: A3DLL.java
+// File description: Implements a Doubly Linked List, as
+// defined in the DLLADT interface, using the DLLNode class.
 // Other files in this project: 
+// * DLLADT.java
+// * DLLNode.java
+// * DLLTesterInteger.java
+// 
 // Challenges: 
 // Time Spent: 
 // Specified Output:
 // --------------------         
 // 
+// The DLL looks like this:  [null] [null] 
+// Inserted Last: 11. The DLL looks like this:  [null] [11] [null] 
+// Inserted Last: 33. The DLL looks like this:  [null] [11] [33] [null] 
+// Inserted Last: 55. The DLL looks like this:  [null] [11] [33] [55] [null] 
+// There are  3 items in the list. First:    11. Last:    55. 
+// Removed First: 11. The DLL looks like this:  [null] [33] [55] [null] 
+// Removed Last: 55. The DLL looks like this:  [null] [33] [null] 
+// There are  1 items in the list. 
+// Is the list empty?  false
+// Inserted First: 22. The DLL looks like this:  [null] [22] [33] [null] 
+// Inserted Last: 44. The DLL looks like this:  [null] [22] [33] [44] [null] 
+// Inserted First: 66. The DLL looks like this:  [null] [66] [22] [33] [44] [null] 
+// There are  4 items in the list. First:    66. Last:    44. 
+// 
+// Removed First Item: 66. The DLL looks like this:  [null] [22] [33] [44] [null] 
+// Removed Last Item: 44. The DLL looks like this:  [null] [22] [33] [null] 
+// There are  2 items in the list. 
+// Removed First Item: 22. The DLL looks like this:  [null] [33] [null] 
+// Removed Last Item: 33. The DLL looks like this:  [null] [null] 
+// Is the list empty?  true
+// BUILD SUCCESSFUL (total time: 0 seconds)
 // 
 // Actual Output:
 // --------------------  
 // 
 //                   Revision History
-//
+// 
 // Date:                   By:                  Action:
 // ----------------------------------------------------
-// XXXX-XX-XX              HH              what you did
+// 2018-03-03              HH              Created file
+// 2018-03-03              HH     Completed code header
 package A3DLL;
 
 /**
- *
- * @author henry
+ * Implements a Doubly Linked List, as defined in the
+ * {@link DLLADT} interface, using the {@link DLLNode} class.
+ * @author Henry Hedden
  */
 class DLL<T> {
 	
