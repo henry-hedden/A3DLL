8fff1666b44e9a5c9b61ee1b6d672c04d5c2f9ca
Henry Hedden <hhedden1@mail.middlesex.edu>
2018-03-03 14:44:18 -0500

    Updated code header with sample output from A3DLLTesterIntegerOutput2018.docx.

--- src/A3DLL/DLL.java
+++ src/A3DLL/DLL.java
@@ -15,25 +15,27 @@
 // Specified Output:
 // --------------------         
 // 
-// The DLL looks like this:  [null] [null] 
-// Inserted Last: 11. The DLL looks like this:  [null] [11] [null] 
-// Inserted Last: 33. The DLL looks like this:  [null] [11] [33] [null] 
-// Inserted Last: 55. The DLL looks like this:  [null] [11] [33] [55] [null] 
+// DLLTesterInteger Output:
+
+// The DLL looks like this:  EMPTY
+// Inserted Last: 11. The DLL looks like this:  [11] 
+// Inserted Last: 33. The DLL looks like this:  [11] [33] 
+// Inserted Last: 55. The DLL looks like this:  [11] [33] [55] 
 // There are  3 items in the list. First:    11. Last:    55. 
-// Removed First: 11. The DLL looks like this:  [null] [33] [55] [null] 
-// Removed Last: 55. The DLL looks like this:  [null] [33] [null] 
+// Removed First: 11. The DLL looks like this:  [33] [55] 
+// Removed Last: 55. The DLL looks like this:  [33] 
 // There are  1 items in the list. 
 // Is the list empty?  false
-// Inserted First: 22. The DLL looks like this:  [null] [22] [33] [null] 
-// Inserted Last: 44. The DLL looks like this:  [null] [22] [33] [44] [null] 
-// Inserted First: 66. The DLL looks like this:  [null] [66] [22] [33] [44] [null] 
+// Inserted First: 22. The DLL looks like this:  [22] [33] 
+// Inserted Last: 44. The DLL looks like this:  [22] [33] [44] 
+// Inserted First: 66. The DLL looks like this:  [66] [22] [33] [44] 
 // There are  4 items in the list. First:    66. Last:    44. 
 // 
-// Removed First Item: 66. The DLL looks like this:  [null] [22] [33] [44] [null] 
-// Removed Last Item: 44. The DLL looks like this:  [null] [22] [33] [null] 
+// Removed First Item: 66. The DLL looks like this:  [22] [33] [44] 
+// Removed Last Item: 44. The DLL looks like this:  [22] [33] 
 // There are  2 items in the list. 
-// Removed First Item: 22. The DLL looks like this:  [null] [33] [null] 
-// Removed Last Item: 33. The DLL looks like this:  [null] [null] 
+// Removed First Item: 22. The DLL looks like this:  [33] 
+// Removed Last Item: 33. The DLL looks like this:  EMPTY
 // Is the list empty?  true
 // BUILD SUCCESSFUL (total time: 0 seconds)
 // 
@@ -112,7 +114,7 @@ class DLL<T> implements DLLADT<T> {
 	 */
 	@Override
 	public T getFirstItem() throws NoSuchElementException {
-		//To change body of generated methods, choose Tools | Templates.
+		// To change body of generated methods, choose Tools | Templates.
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
 
@@ -123,7 +125,7 @@ class DLL<T> implements DLLADT<T> {
 	 */
 	@Override
 	public T getLastItem() throws NoSuchElementException {
-		//To change body of generated methods, choose Tools | Templates.
+		// To change body of generated methods, choose Tools | Templates.
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
 
@@ -133,7 +135,7 @@ class DLL<T> implements DLLADT<T> {
 	 */
 	@Override
 	public void insertFirstItem(Object element) {
-		//To change body of generated methods, choose Tools | Templates.
+		// To change body of generated methods, choose Tools | Templates.
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
 
@@ -143,7 +145,7 @@ class DLL<T> implements DLLADT<T> {
 	 */
 	@Override
 	public void insertLastItem(Object element) {
-		//To change body of generated methods, choose Tools | Templates.
+		// To change body of generated methods, choose Tools | Templates.
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
 
@@ -154,7 +156,7 @@ class DLL<T> implements DLLADT<T> {
 	 */
 	@Override
 	public T removeFirstItem() throws NoSuchElementException {
-		//To change body of generated methods, choose Tools | Templates.
+		// To change body of generated methods, choose Tools | Templates.
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
 
@@ -165,7 +167,7 @@ class DLL<T> implements DLLADT<T> {
 	 */
 	@Override
 	public T removeLastItem() throws NoSuchElementException {
-		//To change body of generated methods, choose Tools | Templates.
+		// To change body of generated methods, choose Tools | Templates.
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
 	
